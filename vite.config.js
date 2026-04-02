fdbf1dfe5d9a909dd1659599ce6ec373282e09b3
Medipelly Harshitha <23211A04E7@bvrit.ac.in>
2026-04-02 12:55:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Finance-Dashboard-UI/',
+  base: "/finance-dashboard/",   // ⭐ THIS FIXES YOUR ISSUE
 })
